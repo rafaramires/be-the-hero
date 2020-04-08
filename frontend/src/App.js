c4087c1ca55e2e38073e2cedf9834ae633d6e3b4
Rafael Ramires <rafaelramires@MacBook-Air-de-Rafael.local>
2020-04-07 23:00:07 -0300

    Front-end do App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello Omnistack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
